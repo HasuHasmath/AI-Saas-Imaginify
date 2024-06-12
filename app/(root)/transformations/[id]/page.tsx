3d183abc95201c26aa25101ae3f9af3c60ceb8cb
hasuhasmath <hasuh5pepe@gmail.com>
2024-06-12 16:16:53 +0530

    end of page routing

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TransformationsPage = () => {
+  return (
+    <div>
+      <div>transformation</div>
+    </div>
+  )
+}
+
+export default TransformationsPage
+
+// localhost:3000/transformations
+// localhost:3000/transformations/[id]
